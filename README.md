5a0251fdd1c8b9e9bae2c0a9d8b238217fb7a75f
KatherineZh000 <yaodanzhang00@gmail.com>
2024-01-31 03:07:34 -0600

    ptf-algo

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# bptf-algo
+# Project Report: Predictive Modeling for Crop Prices in Online Indian Markets with Sparse Historical Data
+
+Note: This GitHub repository is confidential for read only, any extraction must be approved by the author.
+
+## Introduction
+
+This project aims to predict future prices for approximately 150 crops across 200 online Indian markets, using a sparse historical transaction data spanning from 2014 to 2019. We utilized three datasets: Transaction Data, containing daily transaction data including crop and mandi IDs, seller and buyer IDs, crop quantity, and crop traded price; Mandi Characteristics, providing mandi details such as name, district, state, location, and market type (open market or not); and Crop Characteristics, mapping crop names to IDs. Here's a summary of each dataset:
+
+![dataset_first_part] ./data1.png
+
+![dataset_second_part] ./data2.png
+
+Sparse transaction data poses a challenge as price information may not be available for every crop in every mandi daily due to farmers' irregular visits and seasonal crop availability. To create precise one-day ahead price predictions for each crop in every market, we first conduct exploratory data analysis and employ missing-value imputation techniques. We then select predictive models based on four machine learning techniques (the main one called [PTF](https://www.cs.cmu.edu/~jgc/publication/PublicationPDF/Temporal_Collaborative_Filtering_With_Bayesian_Probabilidtic_Tensor_Factorization.pdf)'s development is spearheaded in this project) and evaluate their performance. Throughout the project, we employ a train-test validation method to assess model accuracy.
+
+## Part I. Exploratory Data Analysis
+
+In the first part, we addressed key questions to guide our selection of the prediction model:
+
+1. [ ] We investigated whether geographically proximate markets exhibit correlated crop prices.
+2. [ ] We examined if certain markets demonstrate higher price variability compared to others.
+3. [ ] We identified if there exists a dominant market influencing other markets.
+4. [ ] We explored correlations among crops akin to market correlations.
+
+---
+
+For our first question, we grouped markets by district using district IDs. Then, we identified the most traded crop in each district based on transaction data from 2014 to 2019. Next, we selected the two markets trading this crop most frequently in this district. Using intersected transaction dates, we calculated the correlation coefficient between these two markets' prices for the chosen crop over time. This process was implemented in `PriceCorrOverCloseMarkets()` in `part1.py`. With output as follows:
+
+```
+Correlation between mandi 3 and mandi 9 over crop 17 is 0.8082058671768368.
+Correlation between mandi 4 and mandi 22 over crop 6 is 0.6588707894950585.
+Correlation between mandi 63 and mandi 187 over crop 4 is 0.5933894315791544.
+Correlation between mandi 16 and mandi 117 over crop 6 is 0.47967122154626196.
+Correlation between mandi 122 and mandi 126 over crop 4 is 0.5288967137844734.
+Correlation between mandi 147 and mandi 25 over crop 4 is 0.5426776295567428.
+Correlation between mandi 33 and mandi 41 over crop 2 is 0.8322216322594749.
+Correlation between mandi 150 and mandi 152 over crop 21 is 0.6493325813182255.
+The average price correlation over 8 pairs of close markets is 0.6366582333395285.
+```
+
+Our analysis revealed that out of the eight pairs of markets where transaction data allowed correlation calculation, all but one exhibited correlations exceeding 0.5, with two pairs exceeding 0.8, indicating a notably strong positive correlation. On average, correlations hovered around 0.63. Thus, we concluded that geographically proximate markets tend to demonstrate price correlation for their respective crops.
+
+For our second question, we conducted an exploration on a mandi-by-mandi basis. Specifically, for each mandi, we identified the most traded crop and calculated the variance of its daily weighted price. This procedure was implemented in `MarketVariability ()` in `part1.py`. The output is:
+
+```
+Of all markets, the minimum price variance for a single market is 0.4510640032548529, the 25th quantile price variance is 13.712867783124935, median price variance is 30.823880561057084, the 75th quantile price variance is 97.777898403187, the maximum price variance is 185.79842023464707.
+```
+
+We observed a 75th quantile variance of 97.77, while a maximum variance of 185.79. Therefore, it is reasonable to deduce that, some markets are much more variable than the others.
+
+For our third question, we examined the impact of a particular market on all other markets. For each market, we identified the crop traded most frequently and calculated its weighted price across all other markets. Subsequently, we conducted a correlation analysis between the price of this market's crop (shifted one day ahead) and the weighted crop price of all other markets, determining if this market exerted influence on others. Analysis was conducted using intersected transaction dates and implemented in `InfluentialMarket ()` in `part1.py`.
+
+The maximum correlation achieved through this method was 0.51, attributed to mandi 22. However, upon examining the correlation table between mandi 22 and other mandis for crop 6's price, we observed that only data from the years 2017 and 2018 were utilized, indicating a limited temporal correlation strength. We also analyzed a correlation table for mandi 3 and other mandis regarding crop 2's price:
+
+```
+The price correlation table over crop 2 between market 3 and other markets is
+          date  weighted_price_x  weighted_price_y
+0   2015-12-13               5.0          5.838832
+1   2015-12-18               7.0          7.441011
+2   2015-12-20               7.0         10.615118
+3   2015-12-26               9.0         10.000000
+4   2015-12-29              11.0         10.773043
+..         ...               ...               ...
+577 2018-07-21               9.0          7.965580
+578 2018-07-22               9.0          7.442310
+579 2018-07-23               7.0          8.912302
+580 2018-07-24               5.0         11.300996
+581 2018-07-25              15.0          9.652697
+
+[582 rows x 3 columns]
+Correlation equals 0.48521514609447636.
+```
+
+This correlation demonstrated robustness over time, spanning from 2015 to 2018 across 582 days, with a value just slightly below 0.5. However, we noted that the price for this crop remained relatively stable around 8.0. Given these observations, we inferred that this crop might be a daily cooking necessity, which is traded frequently with a consistently low price. Consequently, concluding that mandi 3 is an influential market would be inappropriate, as all markets may exhibit similar price fluctuations for such an essential crop.
+
+For our final question, we identified the top 5 traded crops based on trading days and conducted correlations for every pair of them if their intersected trading days exceeded 400. This task was completed in `CropCorrelation ()` in `part1.py`.
+
+Among the outputed 6 pairs of crops, 1 pair has no correlation, 3 pairs have slightly positive price correlation, 1 pair has slightly negative price correlation, and 1 pair has a strong positve correlation (crop 4 and crop 9) with a Pearson correlation of 0.63.
+
+## Part II. Prediction Models Selection and Evaluation
+
+In the second part, we aim to construct imputation models to address the missing values mentioned earlier, facilitating price prediction. Following our data exploration insights, we identified that crop prices correlate strongly with categorical traits like mandi location and crop type. Consequently, we selected two models, Classification and Regression Trees (CART) and Random Forest regressor (RF), for category-based predictions. Another model we chose is Temporal Collaborative Filtering using Probabilistic Tensor Factorization (PTF), which was built on Equation 3.9 of the paper "[Temporal Collaborative Filtering with Bayesian Probabilistic Tensor Factorization](https://www.cs.cmu.edu/~jgc/publication/PublicationPDF/Temporal_Collaborative_Filtering_With_Bayesian_Probabilidtic_Tensor_Factorization.pdf)". The reason for choosing PTF is that it deals with time-related sparse data, which perfectly suits our purpose. Since markets are commonly sensitive to their previous prices, another model we chose is simply the past mean price of the crops if available; if not, we filled it with the average price of all crops. The last model we chose is the Multi-Layer Perceptron regressor, one of the fundamental convolutional neural networks (CNN).
+
+In our train-test validation setup, we divided the transaction data from 2014 to 2018 for training and data from 2019 for testing. Treating dates as "month-day" for consistency across years, we utilized the weighted price of a crop for the same month, day, and mandi, regardless of the year. For CART, RF, and CNN, we selected categories including "date no year", "crop ID", "mandi ID", "District ID", "Latitude", "Longitude", and "Type" (1 for open market, 0 for null). Additionally, for PTF, we split the training sets into eight subsets based on "date no year", "mandi ID", and "crop ID" to enhance model accuracy; these subsets were used to construct mandi features, crop features, and time features as stated by Eq. 3.9 in the [paper](https://www.cs.cmu.edu/~jgc/publication/PublicationPDF/Temporal_Collaborative_Filtering_With_Bayesian_Probabilidtic_Tensor_Factorization.pdf). Consequently, the testing sets were also split into eight subsets using the same criteria, and the MAE of all five models were assessed within each subset. Finally, we evaluated the average MAE across the entire testing set. The whole process was implemented in `part2.py` with the following results.
+
+![result] ./result.png
+
+We found that among the seven rounds where the eighth round has an empty testing dataset, PTF's performance remains comparable to the tree and the mean techniques, and is most of the time better than the performance of CNN. One exception is in round 3, where the instability of its performance might come from a bad approximation of the optimal log likelihood in the model, because this likelihood function is non-convex and therefore ”SLSQP” in the minimizer function only loops to a local but not global minimum. But one potential gain using collaborative filtering is that, the user and item features can be applied to similar scenarios when historical data of the users over an item is completely unavailable. In this case, past mean is no longer available. Apart from that, PTF can capture the possible trends with time using its time layer/feature, but in our setting mean and trees seemed to be good enough for this aspect.
+
+## Part III. Discussion
+
+Time division is another crutial factor that matters in model performance, as here we chose year 2014 to 2018's data to be the training set and year 2019 to be the testing set. This 5:1 train-test ratio is good enough for a typical machine learning technique, but what if the traning data becomes more and more sparse? Future evaluations can focus on the impact of sparsity threshold on model performance and potentially demonstrate the superiority of the PTF technique.
+
+## Reference
+
+Xiong, Liang, et al. "Temporal collaborative filtering with bayesian probabilistic tensor factorization."  *Proceedings of the 2010 SIAM international conference on data mining* . Society for Industrial and Applied Mathematics, 2010.
---
+++ part2.py
@@ -0,0 +1,335 @@
+import pandas as pd
+import numpy as np
+from scipy import optimize
+import time
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+
+# Set timer.
+t1 = time.time()
+
+# Load data sets.
+df1 = pd.read_csv('Transaction_Data.csv')
+df1['date'] = pd.to_datetime(df1['date'])
+df1['date_no_year'] = [int(i) for i in df1['date'].dt.strftime("%m%d")]
+df1['income'] = df1['quantity'] * df1['price']
+
+df2 = pd.read_csv('Mandi_Characteristics.csv')
+df2.rename({'MandiId':'mandi_id'},axis=1,inplace=True)
+df2['Type'] = df2['Type'].fillna(0)
+df2['Type'] = df2['Type'].replace('Open Market',1)
+
+# The PTF model constructed based on Eq 3.9 in the paper Temporal Collaborative Filtering with Bayesian Probabilistic Tensor Factorization.
+class PTF():
+    def preprocess_data (self):
+        cropId_max = np.max(np.array(df1['crop_id']))
+        cropId_median = cropId_max // 2
+        mandiId_max = np.max(np.array(df1['mandi_id']))
+        mandiId_median = mandiId_max // 2
+
+        # Divide dates into 2 zones.
+        dates1 = np.array([int(i) for i in pd.date_range("2016-01-01", "2016-06-30").strftime("%m%d")])
+        dates2 = np.array([int(i) for i in pd.date_range("2016-07-01", "2016-12-31").strftime("%m%d")])
+
+        # Divide crops into 2 zones.
+        crops1 = np.array(range(1, cropId_median)) 
+        crops2 = np.array(range(cropId_median, cropId_max+1)) 
+
+        # Divide mandis into 2 zones.
+        mandis1 = np.array(range(1, mandiId_median)) 
+        mandis2 = np.array(range(mandiId_median, mandiId_max+1)) 
+
+        # Collect zones for dates, crops, mandis
+        dates = [dates1, dates2]
+        crops = [crops1, crops2]
+        mandis = [mandis1, mandis2]
+        return dates, crops, mandis
+
+    def __init__(self):
+        self.d = 2
+        self.alpha = 0.1
+        self.sigmaU = 2
+        self.sigmaV = 2
+        self.sigmaT = 2
+        self.sigmaT0 = 2
+        self.miuT0 = np.array([0]*self.d)
+        self.dates, self.crops, self.mandis = self.preprocess_data ()
+
+    # Use data before year 2019 as training set.
+    def preprocess_training_sets(self):
+        train = df1.loc[df1['date'] < '2019-01-01'] 
+        train['total_income'] = train.groupby(['date_no_year', 'crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
+        train['total_quantity'] = train.groupby(['date_no_year', 'crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
+        train['weighted_price'] = train['total_income'] / train['total_quantity']
+        train = train.drop_duplicates(subset = ['date_no_year', 'crop_id', 'mandi_id'])[['date_no_year', 'crop_id', 'mandi_id', 'weighted_price']]
+
+        for i in range(2):
+            globals()[f'train{i+1}'] = train.loc[train['date_no_year'].isin(self.dates[i])]
+        for j in range(1,3):
+            globals()[f'train{j}_1'] = globals()[f'train{j}'].loc[globals()[f'train{j}']['crop_id'].isin(self.crops[0])]
+            globals()[f'train{j}_2'] = globals()[f'train{j}'].loc[globals()[f'train{j}']['crop_id'].isin(self.crops[1])]
+        for k in range(1, 3):
+            for l in range(1, 3):
+                temp = globals()[f'train{k}_{l}']
+                globals()[f'train{k}_{l}_1'] = temp.loc[temp['mandi_id'].isin(self.mandis[0])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
+                globals()[f'train{k}_{l}_2'] = temp.loc[temp['mandi_id'].isin(self.mandis[1])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
+
+    # Use year 2019's data as testing set.
+    def preprocess_testing_sets(self):
+        test = df1.loc[df1['date'] >= '2019-01-01']
+        test['total_income'] = test.groupby(['date_no_year', 'crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
+        test['total_quantity'] = test.groupby(['date_no_year', 'crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
+        test['weighted_price'] = test['total_income'] / test['total_quantity']
+        test = test.drop_duplicates(subset = ['date_no_year', 'crop_id', 'mandi_id'])[['date_no_year', 'crop_id', 'mandi_id', 'weighted_price']]
+
+        for i in range(2):
+            globals()[f'test{i+1}'] = test.loc[test['date_no_year'].isin(self.dates[i])]
+        for j in range(1,3):
+            globals()[f'test{j}_1'] = \
+                globals()[f'test{j}'].loc[globals()[f'test{j}']['crop_id'].isin(self.crops[0])]
+            globals()[f'test{j}_2'] = \
+                globals()[f'test{j}'].loc[globals()[f'test{j}']['crop_id'].isin(self.crops[1])]
+        for k in range(1, 3):
+            for l in range(1, 3):
+                temp = globals()[f'test{k}_{l}']
+                globals()[f'test{k}_{l}_1'] = temp.loc[temp['mandi_id'].isin(self.mandis[0])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
+                globals()[f'test{k}_{l}_2'] = temp.loc[temp['mandi_id'].isin(self.mandis[1])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
+
+    # Construct matrix R in Eq 3.9 of the paper.
+    def construct_R(self, train_or_test, quarterzone, cropzone, mandizone):
+        dates, crops, mandis = self.preprocess_data ()
+
+        date = dates[quarterzone-1]
+        crop = crops[cropzone-1]
+        mandi = mandis[mandizone-1]
+
+        self.num_dates = date.size; self.num_crops = crop.size; self.num_mandis = mandi.size
+        tmp1 = np.repeat(date, self.num_crops * self.num_mandis)
+        tmp2 = np.array([np.repeat(crop, self.num_mandis)] * self.num_dates).flatten()
+        tmp3 = np.array([mandi] * self.num_dates * self.num_crops).flatten()
+        tmp_R = pd.DataFrame()
+        tmp_R['date_no_year'] = tmp1
+        tmp_R['crop_id'] = tmp2
+        tmp_R['mandi_id'] = tmp3
+
+        # 0 stands for train, 1 stands for test
+        if train_or_test == 0: 
+            tmp_R = tmp_R.merge(globals()[f'train{quarterzone}_{cropzone}_{mandizone}'], how='left', on=['date_no_year', 'crop_id', 'mandi_id'])
+        elif train_or_test == 1:
+            tmp_R = tmp_R.merge(globals()[f'test{quarterzone}_{cropzone}_{mandizone}'], how='left', on=['date_no_year', 'crop_id', 'mandi_id'])
+        
+        self.R = np.array(tmp_R['weighted_price']).reshape(self.num_dates, self.num_crops, self.num_mandis)
+        self.R_indi = np.where(np.isnan(self.R)==True, 0, 1)
+        return self.R
+    
+    # Compute the log likelihood in Eq 3.9 of the paper.
+    def log_likelihood(self, x): 
+        sum = 0
+        x = x.reshape(self.num_dates + self.num_crops + self.num_mandis + 1, self.d)
+        U = x[0: self.num_crops]
+        V = x[self.num_crops: self.num_mandis+self.num_crops]
+        T = x[self.num_mandis+self.num_crops: self.num_mandis+self.num_crops+self.num_dates]
+        T0 = x[-1]
+        
+        arr = np.array(np.repeat(0, self.num_dates*self.num_crops*self.num_mandis).\
+            reshape(self.num_dates, self.num_crops, self.num_mandis))
+        
+        for i in range(self.d):
+            t = T[:,i]
+            u = U[:,i]
+            v = V[:,i]
+            tmp = (np.outer(t, u)[:, :, None] * v)
+            tmp.shape
+            arr = tmp + arr
+        
+        self.R_copy = np.nan_to_num(self.R)
+        
+        sum += ((self.R_indi * (self.R_copy-arr))**2).sum()
+        sum += np.square(U).sum()/(self.alpha * self.sigmaU**2 * 2)
+        sum += np.square(V).sum()/(self.alpha * self.sigmaV**2 * 2)
+        sum += (((T[1:]-T[:-1])**2).sum() + ((T[0] - T0)**2).sum())/(self.alpha * self.sigmaT**2 * 2)
+        sum += ((T0-self.miuT0)**2).sum()/(self.alpha * self.sigmaT0**2 * 2)
+        # Print the log likelihood in every optimization step
+        # print(sum) 
+        return sum
+    
+    def minimizer(self, **kwargs):
+        minimum = optimize.minimize(self.log_likelihood, np.array([2]*(self.num_crops + self.num_mandis + self.num_dates + 1) * self.d).\
+            reshape((self.num_crops + self.num_mandis + self.num_dates + 1) * self.d), **kwargs)
+        return minimum
+    
+    # Calculate MAE using PTF.
+    def MAE(self,test,minimum):
+        x = minimum.x
+        R_test_indi = np.where(np.isnan(test) == True, 0, 1)
+        test = np.nan_to_num(test)
+
+        if R_test_indi.sum() != 0:
+            x = x.reshape(self.num_dates + self.num_crops + self.num_mandis + 1, self.d)
+            U_star = x[0: self.num_crops]
+            V_star = x[self.num_crops: self.num_crops + self.num_mandis]
+            T_star = x[self.num_crops + self.num_mandis : self.num_crops + self.num_mandis + self.num_dates]
+            arr = np.array(np.repeat(0, self.num_dates * self.num_crops * self.num_mandis).\
+                    reshape(self.num_dates, self.num_crops, self.num_mandis))
+            
+            for i in range(self.d):
+                t = T_star[:,i]
+                u = U_star[:,i]
+                v = V_star[:,i]
+                tmp = (np.outer(t, u)[:, :, None] * v)
+                tmp.shape
+                arr = tmp + arr
+
+            return (R_test_indi * abs(arr - test)).sum()/R_test_indi.sum()
+        else:
+            return None
+        
+# The classification and regression tree model.
+class CART():
+    def __init__(self):
+        train = df1.loc[df1['date'] < '2019-01-01']
+        train['total_income'] = train.groupby(['date_no_year','crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
+        train['total_quantity'] = train.groupby(['date_no_year','crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
+        train['weighted_price'] = train['total_income'] / train['total_quantity']
+        train = train.drop_duplicates(subset = ['date_no_year','crop_id', 'mandi_id'])
+        train = train.merge(df2[['mandi_id','DistrictId','Lat','Lon','Type']], how='left', on=['mandi_id'])
+        self.train = train.fillna(0)
+
+    def preprocess_testing_sets(self):
+        for i in range(1,3):
+            for j in range(1, 3):
+                for k in range(1, 3):
+                    globals()[f'test{i}_{j}_{k}'] = globals()[f'test{i}_{j}_{k}'].merge(df2[['mandi_id','DistrictId','Lat','Lon','Type']], how='left', on=['mandi_id']).fillna(0)
+
+    def regression_tree(self):
+        self.X = np.array(self.train[['date_no_year','crop_id','mandi_id','DistrictId','Lat','Lon','Type']])
+        self.y = np.array(self.train['weighted_price'])
+        model = tree.DecisionTreeRegressor()
+        self.clf = model.fit(self.X, self.y)
+
+    def MAE(self, i, j, k):
+        result = self.clf.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        error = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        return error
+
+# The mean method.
+class Mean() :
+    def preprocess_data (self):
+        train_for_mean = df1.loc[df1['date'] < '2019-01-01']
+        train_for_mean['total_income'] = train_for_mean.groupby(['crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
+        train_for_mean['total_quantity'] = train_for_mean.groupby(['crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
+        train_for_mean['weighted_price'] = train_for_mean['total_income'] / train_for_mean['total_quantity']
+
+        mean_price = train_for_mean['income'].sum() / train_for_mean['quantity'].sum()
+        train_for_mean = train_for_mean.drop_duplicates(subset=['crop_id','mandi_id'])[['crop_id', 'mandi_id', 'weighted_price']]
+        return train_for_mean, mean_price
+
+    def __init__(self):
+        self.train_for_mean, self.mean_price = self.preprocess_data()
+
+    # Calculate MAE for the mean method.
+    def MAE (self, i, j, k):
+        tmp = globals()[f'test{i}_{j}_{k}'].merge(self.train_for_mean, how='left', on=['crop_id', 'mandi_id'])
+        tmp['weighted_price_y'] = tmp['weighted_price_y'].fillna(self.mean_price)
+        err = abs(tmp['weighted_price_x']-tmp['weighted_price_y']).mean()
+        return err
+
+# The random forest regressor model.
+class RF():
+    def __init__(self, X ,y):
+        self.X = X
+        self.y = y
+        self.regr = RandomForestRegressor(random_state=0).fit(self.X, self.y)
+    
+    def MAE (self,i,j,k):
+        result = self.regr.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        err = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        return err
+
+# The convolutional neural networks model.    
+class CNN():
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        self.regr = MLPRegressor(random_state=1, max_iter=500).fit(self.X, self.y)
+    
+    def MAE (self,i,j,k):
+        result = self.regr.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        err = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        return err
+
+def main():
+    ptf = PTF()
+    ptf.preprocess_training_sets()
+    ptf.preprocess_testing_sets()
+    
+    cart = CART()
+    cart.preprocess_testing_sets()
+    cart.regression_tree()
+
+    mean = Mean()
+
+    # Build a random forest regressor using the same data sets as in CART.
+    rf = RF(cart.X, cart.y)
+
+    # Build a multi-layer perceptron regressor using the same data sets as in CART.
+    cnn = CNN(cart.X, cart.y)
+    
+    errs_Mean = []
+    errs_PTF = []
+    errs_CART = []
+    errs_RF = []
+    errs_CNN = []
+
+    errs_total = []
+
+    # Iterates through time zones.
+    for i in range(1,3): 
+        # Iterates through crop zones.
+        for j in range(1,3): 
+            # Iterates through mandi zones.
+            for k in range(1,3):
+                if not globals()[f'test{i}_{j}_{k}'].empty:
+                    errs_Mean.append(mean.MAE (i, j ,k))
+
+                    test = ptf.construct_R(1,i,j,k)
+                    ptf.construct_R(0,i,j,k)
+                    minimum = ptf.minimizer(method='SLSQP', options={'maxiter' : 18,'maxfev': 18})
+                    errs_PTF.append(ptf.MAE(test,minimum))
+
+                    errs_CART.append(cart.MAE (i, j, k))
+                    errs_RF.append(rf.MAE(i,j,k))
+                    errs_CNN.append(cnn.MAE(i,j,k))
+                    errs_total.append(len(globals()[f'test{i}_{j}_{k}'].index))
+
+    total = np.array(errs_total).sum()
+
+    print("MAE using CART is",(np.array(errs_CART) * np.array(errs_total)).sum()/total,
+        "\nMAE using PTF is",(np.array(errs_PTF)*np.array(errs_total)).sum()/total,
+        "\nMAE using Mean is",(np.array(errs_Mean)*np.array(errs_total)).sum()/total,
+        "\nMAE using random forest is", (np.array(errs_RF)*np.array(errs_total)).sum()/total,
+        "\nMAE using multi-layer perceptron regressor is", (np.array(errs_CNN)*np.array(errs_total)).sum()/total)
+    
+    t2 = time.time()
+    print('Elapsed time is', t2-t1, 's.')
+    
+    # Visualize the MAEs for different models.
+    names = list(range(len(errs_Mean)))
+    x_axis = np.arange(len(names))
+    
+    plt.bar(x_axis - 0.4, errs_CART, width = 0.2, label = 'MAE_CART')
+    plt.bar(x_axis - 0.2, errs_PTF, width = 0.2, label = 'MAE_PTF')
+    plt.bar(x_axis + 0.0, errs_Mean, width = 0.2, label = 'MAE_Mean')
+    plt.bar(x_axis + 0.2, errs_RF, width = 0.2, label = 'MAE_RF')
+    plt.bar(x_axis + 0.4, errs_CNN, width = 0.2, label = 'MAE_CNN')
+    
+    plt.xticks(x_axis, names)
+    
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
