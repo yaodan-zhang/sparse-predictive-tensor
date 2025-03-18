76f9a2dcfe79359226c1c5b1c91fc48612144217
Yaodan Zhang <138193458+yaodan-zhang@users.noreply.github.com>
2025-03-18 03:30:43 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,13 @@
+# Import packages
 import pandas as pd
 import numpy as np
+
 from scipy import optimize
+
 import time
+
 import matplotlib.pyplot as plt
+
 from sklearn import tree
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
@@ -10,22 +15,29 @@ from sklearn.neural_network import MLPRegressor
 # Set timer.
 t1 = time.time()
 
-# Load data sets.
+# Load transaction data
 df1 = pd.read_csv('Transaction_Data.csv')
+
+# Pre-Process data 
 df1['date'] = pd.to_datetime(df1['date'])
 df1['date_no_year'] = [int(i) for i in df1['date'].dt.strftime("%m%d")]
 df1['income'] = df1['quantity'] * df1['price']
 
+# Load market data
 df2 = pd.read_csv('Mandi_Characteristics.csv')
+
+# Pre-Process data
 df2.rename({'MandiId':'mandi_id'},axis=1,inplace=True)
 df2['Type'] = df2['Type'].fillna(0)
 df2['Type'] = df2['Type'].replace('Open Market',1)
 
-# The PTF model constructed based on Eq 3.9 in the paper Temporal Collaborative Filtering with Bayesian Probabilistic Tensor Factorization.
+# The PTF model based on Eq 3.9 in the paper
 class PTF():
+    
     def preprocess_data (self):
         cropId_max = np.max(np.array(df1['crop_id']))
         cropId_median = cropId_max // 2
+        
         mandiId_max = np.max(np.array(df1['mandi_id']))
         mandiId_median = mandiId_max // 2
 
@@ -45,55 +57,76 @@ class PTF():
         dates = [dates1, dates2]
         crops = [crops1, crops2]
         mandis = [mandis1, mandis2]
+        
         return dates, crops, mandis
 
     def __init__(self):
+        # Initialize parameters
         self.d = 2
         self.alpha = 0.1
         self.sigmaU = 2
         self.sigmaV = 2
         self.sigmaT = 2
         self.sigmaT0 = 2
+        
         self.miuT0 = np.array([0]*self.d)
         self.dates, self.crops, self.mandis = self.preprocess_data ()
 
-    # Use data before year 2019 as training set.
+    # Use data before year 2019 as training set
     def preprocess_training_sets(self):
+        
         train = df1.loc[df1['date'] < '2019-01-01'] 
+
+        # Calculate weighted price
         train['total_income'] = train.groupby(['date_no_year', 'crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
         train['total_quantity'] = train.groupby(['date_no_year', 'crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
         train['weighted_price'] = train['total_income'] / train['total_quantity']
+
+        # Drop duplicate data
         train = train.drop_duplicates(subset = ['date_no_year', 'crop_id', 'mandi_id'])[['date_no_year', 'crop_id', 'mandi_id', 'weighted_price']]
 
         for i in range(2):
             globals()[f'train{i+1}'] = train.loc[train['date_no_year'].isin(self.dates[i])]
+            
         for j in range(1,3):
             globals()[f'train{j}_1'] = globals()[f'train{j}'].loc[globals()[f'train{j}']['crop_id'].isin(self.crops[0])]
             globals()[f'train{j}_2'] = globals()[f'train{j}'].loc[globals()[f'train{j}']['crop_id'].isin(self.crops[1])]
+            
         for k in range(1, 3):
+            
             for l in range(1, 3):
                 temp = globals()[f'train{k}_{l}']
+                
                 globals()[f'train{k}_{l}_1'] = temp.loc[temp['mandi_id'].isin(self.mandis[0])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
                 globals()[f'train{k}_{l}_2'] = temp.loc[temp['mandi_id'].isin(self.mandis[1])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
 
     # Use year 2019's data as testing set.
     def preprocess_testing_sets(self):
         test = df1.loc[df1['date'] >= '2019-01-01']
+
+        # Calculate weighted price
         test['total_income'] = test.groupby(['date_no_year', 'crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
         test['total_quantity'] = test.groupby(['date_no_year', 'crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
         test['weighted_price'] = test['total_income'] / test['total_quantity']
+
+        # Drop duplicates
         test = test.drop_duplicates(subset = ['date_no_year', 'crop_id', 'mandi_id'])[['date_no_year', 'crop_id', 'mandi_id', 'weighted_price']]
 
         for i in range(2):
             globals()[f'test{i+1}'] = test.loc[test['date_no_year'].isin(self.dates[i])]
+            
         for j in range(1,3):
             globals()[f'test{j}_1'] = \
                 globals()[f'test{j}'].loc[globals()[f'test{j}']['crop_id'].isin(self.crops[0])]
+            
             globals()[f'test{j}_2'] = \
                 globals()[f'test{j}'].loc[globals()[f'test{j}']['crop_id'].isin(self.crops[1])]
+            
         for k in range(1, 3):
+            
             for l in range(1, 3):
                 temp = globals()[f'test{k}_{l}']
+                
                 globals()[f'test{k}_{l}_1'] = temp.loc[temp['mandi_id'].isin(self.mandis[0])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
                 globals()[f'test{k}_{l}_2'] = temp.loc[temp['mandi_id'].isin(self.mandis[1])].sort_values(by = ['date_no_year', 'crop_id', 'mandi_id'])
 
@@ -106,9 +139,11 @@ class PTF():
         mandi = mandis[mandizone-1]
 
         self.num_dates = date.size; self.num_crops = crop.size; self.num_mandis = mandi.size
+        
         tmp1 = np.repeat(date, self.num_crops * self.num_mandis)
         tmp2 = np.array([np.repeat(crop, self.num_mandis)] * self.num_dates).flatten()
         tmp3 = np.array([mandi] * self.num_dates * self.num_crops).flatten()
+        
         tmp_R = pd.DataFrame()
         tmp_R['date_no_year'] = tmp1
         tmp_R['crop_id'] = tmp2
@@ -121,12 +156,15 @@ class PTF():
             tmp_R = tmp_R.merge(globals()[f'test{quarterzone}_{cropzone}_{mandizone}'], how='left', on=['date_no_year', 'crop_id', 'mandi_id'])
         
         self.R = np.array(tmp_R['weighted_price']).reshape(self.num_dates, self.num_crops, self.num_mandis)
+        
         self.R_indi = np.where(np.isnan(self.R)==True, 0, 1)
+        
         return self.R
     
-    # Compute the log likelihood in Eq 3.9 of the paper.
+    # Compute the log likelihood in Eq 3.9 of the paper
     def log_likelihood(self, x): 
         sum = 0
+        
         x = x.reshape(self.num_dates + self.num_crops + self.num_mandis + 1, self.d)
         U = x[0: self.num_crops]
         V = x[self.num_crops: self.num_mandis+self.num_crops]
@@ -147,30 +185,40 @@ class PTF():
         self.R_copy = np.nan_to_num(self.R)
         
         sum += ((self.R_indi * (self.R_copy-arr))**2).sum()
+        
         sum += np.square(U).sum()/(self.alpha * self.sigmaU**2 * 2)
         sum += np.square(V).sum()/(self.alpha * self.sigmaV**2 * 2)
+        
         sum += (((T[1:]-T[:-1])**2).sum() + ((T[0] - T0)**2).sum())/(self.alpha * self.sigmaT**2 * 2)
+        
         sum += ((T0-self.miuT0)**2).sum()/(self.alpha * self.sigmaT0**2 * 2)
+        
         # Print the log likelihood in every optimization step
         # print(sum) 
+        
         return sum
     
     def minimizer(self, **kwargs):
+        
         minimum = optimize.minimize(self.log_likelihood, np.array([2]*(self.num_crops + self.num_mandis + self.num_dates + 1) * self.d).\
             reshape((self.num_crops + self.num_mandis + self.num_dates + 1) * self.d), **kwargs)
+        
         return minimum
     
-    # Calculate MAE using PTF.
+    # Calculate mean absolute error in PTF
     def MAE(self,test,minimum):
+        
         x = minimum.x
         R_test_indi = np.where(np.isnan(test) == True, 0, 1)
         test = np.nan_to_num(test)
 
         if R_test_indi.sum() != 0:
             x = x.reshape(self.num_dates + self.num_crops + self.num_mandis + 1, self.d)
+            
             U_star = x[0: self.num_crops]
             V_star = x[self.num_crops: self.num_crops + self.num_mandis]
             T_star = x[self.num_crops + self.num_mandis : self.num_crops + self.num_mandis + self.num_dates]
+            
             arr = np.array(np.repeat(0, self.num_dates * self.num_crops * self.num_mandis).\
                     reshape(self.num_dates, self.num_crops, self.num_mandis))
             
@@ -178,130 +226,191 @@ class PTF():
                 t = T_star[:,i]
                 u = U_star[:,i]
                 v = V_star[:,i]
+                
                 tmp = (np.outer(t, u)[:, :, None] * v)
                 tmp.shape
+                
                 arr = tmp + arr
 
             return (R_test_indi * abs(arr - test)).sum()/R_test_indi.sum()
+        
         else:
             return None
         
-# The classification and regression tree model.
+# Implement a CART model
 class CART():
     def __init__(self):
+        # Load training data set
         train = df1.loc[df1['date'] < '2019-01-01']
+
+        # Calculate weighted price
         train['total_income'] = train.groupby(['date_no_year','crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
         train['total_quantity'] = train.groupby(['date_no_year','crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
         train['weighted_price'] = train['total_income'] / train['total_quantity']
+        
         train = train.drop_duplicates(subset = ['date_no_year','crop_id', 'mandi_id'])
         train = train.merge(df2[['mandi_id','DistrictId','Lat','Lon','Type']], how='left', on=['mandi_id'])
+        
         self.train = train.fillna(0)
 
     def preprocess_testing_sets(self):
         for i in range(1,3):
+            
             for j in range(1, 3):
+                
                 for k in range(1, 3):
+                    
                     globals()[f'test{i}_{j}_{k}'] = globals()[f'test{i}_{j}_{k}'].merge(df2[['mandi_id','DistrictId','Lat','Lon','Type']], how='left', on=['mandi_id']).fillna(0)
 
     def regression_tree(self):
+        
         self.X = np.array(self.train[['date_no_year','crop_id','mandi_id','DistrictId','Lat','Lon','Type']])
         self.y = np.array(self.train['weighted_price'])
+        
         model = tree.DecisionTreeRegressor()
         self.clf = model.fit(self.X, self.y)
 
     def MAE(self, i, j, k):
+        
         result = self.clf.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        
         error = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        
         return error
 
-# The mean method.
+# Mean Model
 class Mean() :
     def preprocess_data (self):
+        # Load training data set
         train_for_mean = df1.loc[df1['date'] < '2019-01-01']
+
+        # Calculate weighted price
         train_for_mean['total_income'] = train_for_mean.groupby(['crop_id', 'mandi_id'])['income'].transform(lambda x: x.sum())
         train_for_mean['total_quantity'] = train_for_mean.groupby(['crop_id', 'mandi_id'])['quantity'].transform(lambda x: x.sum())
         train_for_mean['weighted_price'] = train_for_mean['total_income'] / train_for_mean['total_quantity']
 
+        # Calculate mean price
         mean_price = train_for_mean['income'].sum() / train_for_mean['quantity'].sum()
+
+        # Drop duplicate
         train_for_mean = train_for_mean.drop_duplicates(subset=['crop_id','mandi_id'])[['crop_id', 'mandi_id', 'weighted_price']]
+        
         return train_for_mean, mean_price
 
     def __init__(self):
+        
         self.train_for_mean, self.mean_price = self.preprocess_data()
 
-    # Calculate MAE for the mean method.
+    # Calculate mean absolute error
     def MAE (self, i, j, k):
         tmp = globals()[f'test{i}_{j}_{k}'].merge(self.train_for_mean, how='left', on=['crop_id', 'mandi_id'])
+        
         tmp['weighted_price_y'] = tmp['weighted_price_y'].fillna(self.mean_price)
-        err = abs(tmp['weighted_price_x']-tmp['weighted_price_y']).mean()
+        
+        err = abs(tmp['weighted_price_x'] - tmp['weighted_price_y']).mean()
+        
         return err
 
-# The random forest regressor model.
+# Random Forest Model
 class RF():
     def __init__(self, X ,y):
+        
         self.X = X
         self.y = y
+        
         self.regr = RandomForestRegressor(random_state=0).fit(self.X, self.y)
     
-    def MAE (self,i,j,k):
+    def MAE (self, i, j, k):
+        
         result = self.regr.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        
         err = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        
         return err
 
-# The convolutional neural networks model.    
+# Multi-Layer Perception Neural Network (CNN)
 class CNN():
     def __init__(self, X, y):
+        
         self.X = X
         self.y = y
+        
         self.regr = MLPRegressor(random_state=1, max_iter=500).fit(self.X, self.y)
     
     def MAE (self,i,j,k):
+        
         result = self.regr.predict(np.array(globals()[f'test{i}_{j}_{k}'][['date_no_year','crop_id', 'mandi_id', 'DistrictId','Lat','Lon','Type']]))
+        
         err = np.mean(abs(np.array(globals()[f'test{i}_{j}_{k}']['weighted_price']) - result))
+        
         return err
 
 def main():
+    
+    # Construct PTF model
     ptf = PTF()
     ptf.preprocess_training_sets()
     ptf.preprocess_testing_sets()
-    
+
+    # Construct CART model
     cart = CART()
     cart.preprocess_testing_sets()
     cart.regression_tree()
 
+    # Construct Mean model
     mean = Mean()
 
-    # Build a random forest regressor using the same data sets as in CART.
+    # Construct a RF model with the same datasets in CART
     rf = RF(cart.X, cart.y)
 
-    # Build a multi-layer perceptron regressor using the same data sets as in CART.
+    # Construct a Multi-Layer Perceptron model with the same datasets in CART
     cnn = CNN(cart.X, cart.y)
-    
-    errs_Mean = []
-    errs_PTF = []
-    errs_CART = []
-    errs_RF = []
-    errs_CNN = []
 
+    # Initialize errors
+    errs_Mean = []
+    
+    errs_PTF = []
+    
+    errs_CART = []
+    
+    errs_RF = []
+    
+    errs_CNN = []
+    
     errs_total = []
 
-    # Iterates through time zones.
+    # Iterates through time zones
     for i in range(1,3): 
-        # Iterates through crop zones.
+        
+        # Iterates through crop zones
         for j in range(1,3): 
-            # Iterates through mandi zones.
+            
+            # Iterates through mandi zones
             for k in range(1,3):
+                
                 if not globals()[f'test{i}_{j}_{k}'].empty:
+                    
                     errs_Mean.append(mean.MAE (i, j ,k))
 
                     test = ptf.construct_R(1,i,j,k)
+                    
                     ptf.construct_R(0,i,j,k)
+                    
                     minimum = ptf.minimizer(method='SLSQP', options={'maxiter' : 18,'maxfev': 18})
+
+                    # Calculate PTF error
                     errs_PTF.append(ptf.MAE(test,minimum))
 
+                    # Calculate CART error
                     errs_CART.append(cart.MAE (i, j, k))
+
+                    # Calculate RF error
                     errs_RF.append(rf.MAE(i,j,k))
+
+                    # Calculate CNN error
                     errs_CNN.append(cnn.MAE(i,j,k))
+
+                    # Calculate total error
                     errs_total.append(len(globals()[f'test{i}_{j}_{k}'].index))
 
     total = np.array(errs_total).sum()
@@ -313,23 +422,32 @@ def main():
         "\nMAE using multi-layer perceptron regressor is", (np.array(errs_CNN)*np.array(errs_total)).sum()/total)
     
     t2 = time.time()
+
+    # Elapsed time
     print('Elapsed time is', t2-t1, 's.')
     
-    # Visualize the MAEs for different models.
+    # Visualize MAEs for different models
     names = list(range(len(errs_Mean)))
+    
     x_axis = np.arange(len(names))
     
     plt.bar(x_axis - 0.4, errs_CART, width = 0.2, label = 'MAE_CART')
+    
     plt.bar(x_axis - 0.2, errs_PTF, width = 0.2, label = 'MAE_PTF')
+    
     plt.bar(x_axis + 0.0, errs_Mean, width = 0.2, label = 'MAE_Mean')
+    
     plt.bar(x_axis + 0.2, errs_RF, width = 0.2, label = 'MAE_RF')
+    
     plt.bar(x_axis + 0.4, errs_CNN, width = 0.2, label = 'MAE_CNN')
     
     plt.xticks(x_axis, names)
     
     plt.legend()
+    
     plt.show()
 
-
+# Start main()
 if __name__ == "__main__":
+    
     main()
